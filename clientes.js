89a9e451398763f5468f6c685f9ed61b2f5549ab
srmartinis <sr_martinis@hotmail.com>
2024-07-06 16:05:36 -0300

    se agregan clientes.js y .gitignore y varios cambios mas

---
+++ clientes.js
@@ -0,0 +1,31 @@
+
+
+let clientes = []
+clientes.push({
+    nombre:"Leonardo Fernandez",
+    info:[{
+        cel:"2616589658",
+        otraInfo:"Hijo Mayor"
+    }]
+})
+clientes.push({
+    nombre:"Canela Arias",
+    info:[{
+        cel:"2616911783",
+        otraInfo:"Esposa"
+    }]
+})
+clientes.push({
+    nombre:"Martin Saavedra",
+    info:[{
+        cel:"261669745",
+        otraInfo:"Jefe en Jefes"
+    }]
+})
+clientes.push({
+    nombre:"Valentina Saavedra",
+    info:[{
+        cel:"261656845",
+        otraInfo:"Hijo Menor"
+    }]
+})
